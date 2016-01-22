ccda4e2a81896ea3d136bed897b1041cb0085e56
Wesley Soo-Hoo <wesoohoo@gmail.com>
2016-01-21 23:29:16 -0800

    Add Auto Framework

---
+++ src/org/camsrobotics/frc2016/auto/AutoMode.java
@@ -0,0 +1,14 @@
+package org.camsrobotics.frc2016.auto;
+
+/**
+ * Abstract class of each of the auto modes.
+ * 
+ * @author Wesley
+ *
+ */
+public abstract class AutoMode {
+	/**
+	 * This function will be called during Autonomous
+	 */
+	public abstract void run();
+}

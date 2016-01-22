93aec498032bbcb4dbbfd4db3a7c53d41f00076e
Wesley Soo-Hoo <wesoohoo@gmail.com>
2016-01-22 10:10:33 -0800

    Add the beginnings of the Drivebase interface

--- src/org/camsrobotics/frc2016/subsystems/Drive.java
+++ src/org/camsrobotics/frc2016/subsystems/Drive.java
@@ -1,5 +1,44 @@
 package org.camsrobotics.frc2016.subsystems;
 
-public class Drive {
+import org.camsrobotics.lib.Loopable;
 
+/**
+ * Drivebase Interface
+ * 
+ * @author Wesley
+ *
+ */
+public class Drive implements Loopable {
+	
+	
+	/**
+	 * Send a DriveSignal to the drivebase to control it
+	 * 
+	 * @author Wesley
+	 *
+	 */
+	public class DriveSignal	{
+		public double leftSpeed;
+		public double rightSpeed;
+		
+		public DriveSignal(double leftSpeed, double rightSpeed)	{
+			this.leftSpeed = leftSpeed;
+			this.rightSpeed = rightSpeed;
+		}
+	}
+	
+	/**
+	 * Abstract controller
+	 * 
+	 * @author Wesley
+	 *
+	 */
+	public interface DriveController	{
+		public DriveSignal get();
+	}
+
+	@Override
+	public void update() {
+		//TODO Add the motor controllers...wait for the gearbox class
+	}
 }

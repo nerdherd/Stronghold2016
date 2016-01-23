1eb77721df27ad6e6a35b281fadac23acfd2c66c
Michael Iovine <mike.iovine7@gmail.com>
2016-01-23 14:32:48 -0800

    i forgot to comment

--- src/org/camsrobotics/lib/Logger.java
+++ src/org/camsrobotics/lib/Logger.java
@@ -7,15 +7,28 @@ import java.io.IOException;
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 
+/**
+ * Logger Class
+ * 
+ * @author Michael
+ *
+ */
 public class Logger {
 	private File m_csv;
 	private FileWriter m_fw;
 	private String m_filename;
-
+	
+	/**
+	 * Constructs file logger.
+	 * @param filename
+	 */
 	public Logger(String filename) {
 		m_filename = filename;
 	}
 	
+	/**
+	 * Run to create file to write to.
+	 */
 	public void init() {
 		m_csv = new File(m_filename);
 		try {
@@ -27,6 +40,11 @@ public class Logger {
 		}
 	}
 	
+	/**
+	 * Pass a string to write a message to the file.
+	 * @param message
+	 */
+	//TODO make it so you cant do this if init has not been run
 	public void write(String message) {
 		try {
 			m_fw.write(message);
@@ -35,6 +53,9 @@ public class Logger {
 		}
 	}
 	
+	/**
+	 * Close the file.
+	 */
 	public void close() {
 		try {
 			m_fw.flush();

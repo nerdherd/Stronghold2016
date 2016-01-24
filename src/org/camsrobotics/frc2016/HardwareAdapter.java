1b2cac590122488212213caf6a7f4a730a4f02cb
Wesley Soo-Hoo <wesoohoo@gmail.com>
2016-01-24 14:25:25 -0800

    Add the Shifters

--- src/org/camsrobotics/frc2016/HardwareAdapter.java
+++ src/org/camsrobotics/frc2016/HardwareAdapter.java
@@ -5,6 +5,7 @@ import org.camsrobotics.frc2016.subsystems.Intake;
 import org.camsrobotics.frc2016.subsystems.Shooter;
 import org.camsrobotics.lib.Gearbox;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.VictorSP;
 
 public class HardwareAdapter {
@@ -14,12 +15,16 @@ public class HardwareAdapter {
 	public static final VictorSP kDriveBackLeft		= new VictorSP(2);
 	public static final VictorSP kDriveBackRight	= new VictorSP(2);
 	
+	// Solenoids
+	public static final DoubleSolenoid kLeftShifter	= new DoubleSolenoid(1,2);
+	public static final DoubleSolenoid kRightShifter= new DoubleSolenoid(3,4);
+	
 	// Gearboxes
-	public static final Gearbox kDriveLeftGearbox	= new Gearbox(kDriveFrontLeft, kDriveBackLeft);
-	public static final Gearbox kDriveRightGearbox	= new Gearbox(kDriveFrontRight, kDriveBackRight);
+	public static final Gearbox kDriveLeftGearbox	= new Gearbox(kDriveFrontLeft, kDriveBackLeft, kLeftShifter);
+	public static final Gearbox kDriveRightGearbox	= new Gearbox(kDriveFrontRight, kDriveBackRight, kRightShifter);
 	
 	// Subsystems
-	public static final Drive drive = new Drive(kDriveLeftGearbox, kDriveRightGearbox);
-	public static final Shooter shooter = new Shooter();
-	public static final Intake intake = new Intake();
+	public static final Drive kDrive = new Drive(kDriveLeftGearbox, kDriveRightGearbox);
+	public static final Shooter kShooter = new Shooter();
+	public static final Intake kIntake = new Intake();
 }

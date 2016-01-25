b90e8e04071352cfb3ceb774daefbc23d6f1998c
Wesley Soo-Hoo <wesoohoo@gmail.com>
2016-01-24 16:51:40 -0800

    Add a few sensors

--- src/org/camsrobotics/frc2016/HardwareAdapter.java
+++ src/org/camsrobotics/frc2016/HardwareAdapter.java
@@ -6,7 +6,11 @@ import org.camsrobotics.frc2016.subsystems.Lifter;
 import org.camsrobotics.frc2016.subsystems.Shooter;
 import org.camsrobotics.lib.Gearbox;
 
+import com.kauailabs.navx_mxp.AHRS;
+
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.VictorSP;
 
 public class HardwareAdapter {
@@ -20,9 +24,14 @@ public class HardwareAdapter {
 	public static final DoubleSolenoid kLeftShifter	= new DoubleSolenoid(1,2);
 	public static final DoubleSolenoid kRightShifter= new DoubleSolenoid(3,4);
 	
+	// Sensors
+	public static final AHRS kNavX					= new AHRS(new SerialPort(57600, SerialPort.Port.kMXP));
+	public static final Encoder kDriveLeftEncoder	= new Encoder(0,1);
+	public static final Encoder kDriveRightEncoder	= new Encoder(2,3);
+	
 	// Gearboxes
-	public static final Gearbox kDriveLeftGearbox	= new Gearbox(kDriveFrontLeft, kDriveBackLeft, kLeftShifter);
-	public static final Gearbox kDriveRightGearbox	= new Gearbox(kDriveFrontRight, kDriveBackRight, kRightShifter);
+	public static final Gearbox kDriveLeftGearbox	= new Gearbox(kDriveFrontLeft, kDriveBackLeft, kDriveLeftEncoder, kLeftShifter);
+	public static final Gearbox kDriveRightGearbox	= new Gearbox(kDriveFrontRight, kDriveBackRight, kDriveRightEncoder, kRightShifter);
 	
 	// Subsystems
 	public static final Drive kDrive = new Drive(kDriveLeftGearbox, kDriveRightGearbox);

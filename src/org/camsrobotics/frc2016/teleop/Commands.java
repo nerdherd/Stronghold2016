974f168d1e1303f31581b816cbc05755a7334386
Wesley Soo-Hoo <wesoohoo@gmail.com>
2016-01-29 20:21:40 -0800

    Start Teleop Commands

---
+++ src/org/camsrobotics/frc2016/teleop/Commands.java
@@ -0,0 +1,33 @@
+package org.camsrobotics.frc2016.teleop;
+
+/**
+ * Teleoperated commands
+ * 
+ * @author Wesley
+ *
+ */
+public class Commands {
+	
+	public static enum DriveCommands	{
+		TANK_DRIVE, VISION
+	}
+	
+	public static enum ShooterCommands	{
+		MANUAL, LONG_RANGE, MEDIUM_RANGE, SHORT_RANGE, VISION
+	}
+	
+	public static enum IntakeCommands	{
+		MANUAL, FLOOR_LEVEL, BALL_PICKUP, TUCKED_IN
+	}
+	
+	public static enum RollerCommands	{
+		IDLE, INTAKE, OUTTAKE
+	}
+	
+	public DriveCommands driveCommand = DriveCommands.TANK_DRIVE;
+	public ShooterCommands shooterCommand = ShooterCommands.MANUAL;
+	public IntakeCommands intakeCommand = IntakeCommands.MANUAL;
+	public RollerCommands rollerCommand = RollerCommands.IDLE;
+	
+	public boolean shooting = false;
+}

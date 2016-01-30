3385d9f064c9179edc98933093262d25d516a6c8
Wesley Soo-Hoo <wesoohoo@gmail.com>
2016-01-29 21:01:03 -0800

    Fix strait controller to go strait

--- src/org/camsrobotics/frc2016/subsystems/controllers/DriveStraightController.java
+++ src/org/camsrobotics/frc2016/subsystems/controllers/DriveStraightController.java
@@ -4,6 +4,7 @@ import org.camsrobotics.frc2016.Constants;
 import org.camsrobotics.frc2016.subsystems.Drive.DriveController;
 import org.camsrobotics.frc2016.subsystems.Drive.DriveSensorSignal;
 import org.camsrobotics.frc2016.subsystems.Drive.DriveSignal;
+import org.camsrobotics.lib.NerdyMath;
 import org.camsrobotics.lib.NerdyPID;
 
 /**
@@ -13,17 +14,20 @@ import org.camsrobotics.lib.NerdyPID;
  *
  */
 public class DriveStraightController implements DriveController {
-	private final double m_kP = Constants.kDriveStraightP;
-	private final double m_kI = Constants.kDriveStraightI;
-	private final double m_kD = Constants.kDriveStraightD;
+	private final double m_kP = Constants.kDriveRotationP;
+	private final double m_kI = Constants.kDriveRotationI;
+	private final double m_kD = Constants.kDriveRotationD;
 	
 	private double m_tolerance;
-	private double m_pwr = 0;
+	private double m_rotPower = 0;
+	private double m_straitPower = 0;
 	
 	private NerdyPID m_pidController;
 	
-	public DriveStraightController(double tolerance)	{
+	public DriveStraightController(double tolerance, double power)	{
 		m_tolerance = tolerance;
+		
+		m_straitPower = power;
 		
 		m_pidController = new NerdyPID(m_kP, m_kI, m_kD);
 		m_pidController.setDesired(0);
@@ -31,8 +35,10 @@ public class DriveStraightController implements DriveController {
 	
 	@Override
 	public DriveSignal get(DriveSensorSignal sig) {
-		m_pwr = m_pidController.calculate((sig.yaw + 360) % 360);
-		return new DriveSignal(m_pwr, -m_pwr);
+		m_rotPower = m_pidController.calculate((sig.yaw + 360) % 360);
+		double[] pow = {m_rotPower + m_straitPower, -m_rotPower + m_straitPower};
+		pow = NerdyMath.normalize(pow, false);
+		return new DriveSignal(pow[0], pow[1]);
 	}
 
 	@Override
